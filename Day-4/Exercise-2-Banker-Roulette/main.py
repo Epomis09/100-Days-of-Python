5a840b90026f67755aaf60ba8dfa74d03d9b3285
Farhad Abbasi <farhad.abbasi9@gmail.com>
2022-10-12 22:36:26 +0330

    complete exercise 2 from day 4

---
+++ Day-4/Exercise-2-Banker-Roulette/main.py
@@ -0,0 +1,15 @@
+import random
+# 🚨 Don't change the code below 👇
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+length = len(names)
+random_name = random.randint(0, length-1)
+print(f"{names[random_name]} is going to buy the meal today!")
