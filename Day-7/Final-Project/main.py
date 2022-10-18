ba5f07d7f43569c46c7485f257e6d42a28694a0c
Farhad Abbasi <farhad.abbasi9@gmail.com>
2022-10-18 23:03:32 +0330

    find index of input letter

--- Day-7/Final-Project/main.py
+++ Day-7/Final-Project/main.py
@@ -1,12 +1,18 @@
 import random
 
-words = ["monkey", "cat", "horse", "chicken", "donkey"]
+words = ["monkey", "cat", "horse", "chicken", "elephant"]
 word = random.choice(words)
 print(word)
+
 number_of_letter = len(word)
 guess_word = []
 for number in word:
     guess_word.append("_")
-
-# letter = input("Choose a word: ")
 print(guess_word)
+
+letter_input = input("Choose a word: ")
+
+for letter in word:
+    if letter == letter_input:
+        print(word.index(letter))
+
